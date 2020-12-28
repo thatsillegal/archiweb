dcaaee070b277b789c3311da4d2974063ba66ec2
Yichen Mo <amomorning@gmail.com>
2020-12-28 15:20:28 +0800

    Add image processing

--- front-end/src/examples/image-3d.js
+++ front-end/src/examples/image-3d.js
@@ -2,7 +2,9 @@ import * as THREE from "three";
 import * as ARCH from "@/archiweb"
 
 let scene, renderer, gui;
-// let geoFty, matFty;
+let geoFty, matFty;
+let image;
+let gb = [];
 
 function getImageData( image ) {
   
@@ -25,80 +27,101 @@ function getPixel( imagedata, x, y ) {
 }
 
 function getIllumination(pixel) {
+  Object.keys(pixel).map((key)=>{pixel[key] /= 255});
   const i = 0.3 * pixel.r + 0.59 * pixel.g + 0.11 * pixel.b;
-  return i * (pixel.a/255.0);
+  return i * pixel.a;
 }
-/* ---------- GUI setup ---------- */
-function initGUI() {
-  const control = {
-    import:function () {
-      fileInput.click();
+
+function gcd(a, b){
+  return b?gcd(b, a%b):a;
+}
+
+function getDiff(imagedata, x, y, w, h) {
+  let min = 1, max = 0;
+  for(let i = x; i+2 < x+w; i += 2) {
+    for (let j = y; j+2 < y+h; j += 2) {
+      // console.log('i, j = ', i, j)
+      let pixel = getPixel( imagedata, i, j );
+      let illum = getIllumination(pixel);
+      min = Math.min(illum, min);
+      max = Math.max(illum, max);
     }
   }
-  
-  gui.add(control, 'import');
-  
-  let form = document.createElement('form');
-  form.style.display = 'none';
-  document.body.appendChild(form);
-  
-  let fileInput = document.createElement('input');
-  fileInput.multiple = true;
-  fileInput.type = 'file';
-  fileInput.addEventListener('change', function () {
-    let reader = new FileReader();
-    reader.addEventListener('load', function(event){
-      console.log(event);
-      let contents = event.target.result;
-      const loader = new THREE.ImageLoader();
-      loader.load(
-        // resource URL
-        contents,
-    
-        // onLoad callback
-        function ( image ) {
-          // use the image, e.g. draw part of it on a canvas
-          let imagedata = getImageData(image);
-          let color = getPixel( imagedata, 10, 10 );
-          let i = getIllumination(color);
-  
-          console.log(color);
-          console.log(i);
-        },
-    
-        // onProgress callback currently not supported
-        undefined,
-    
-        // onError callback
-        function () {
-          console.error( 'An error happened.' );
-        }
-      );
-    })
-    reader.readAsDataURL(fileInput.files[0]);
+  return {min, max};
+}
 
-    form.reset();
-    
-  });
-  form.appendChild(fileInput);
+function paintGrid(imagedata, x, y, w, h) {
+  let {min, max} = getDiff(imagedata, x, y, w, h);
+  // console.log(min, max);
+  if(max - min > control.threshold && w > control.minimal && h > control.minimal) {
+    let mw = Math.floor(w / 2);
+    let mh = Math.floor(h / 2);
+    paintGrid(imagedata, x, y, mw, mh);
+    paintGrid(imagedata, x+mw, y, w-mw, mh);
+    paintGrid(imagedata, x, y+mh, mw, h-mh);
+    paintGrid(imagedata, x+mw, y+mh, w-mw, h-mh);
+  } else {
+    let pixel = getPixel( imagedata, x, y );
+    // console.log(pixel)
+    let illum = getIllumination(pixel);
+    let color = new THREE.Color(pixel.r, pixel.g, pixel.b);
+    gb.push(
+    geoFty.Box([x+w/2 - imagedata.width/2, -y-h/2 + imagedata.height/2, 0],
+      [w, h, illum*w], matFty.Matte(color), control.edge)
+    );
+  }
+  
 }
 
 
 /* ---------- create your scene object ---------- */
 function initScene() {
-  // geoFty = new ARCH.GeometryFactory(scene);
-  // matFty = new ARCH.MaterialFactory();
+  geoFty = new ARCH.GeometryFactory(scene);
+  matFty = new ARCH.MaterialFactory();
   
+  const loader = new ARCH.Loader(scene);
+  loader.addGUI(gui);
+  
+  loader.addEventListener('load', (event)=>{
+    image = getImageData(event.object);
+    clear();
+    generateGrid(image);
+  });
   
   
   // refresh global objects
+}
+
+function clear() {
+  gb.forEach((it)=>{
+    it.parent.remove(it);
+  });
+  gb = [];
+}
+
+function generateGrid(imagedata) {
+  let mn = gcd(imagedata.width, imagedata.height);
+  console.log(mn)
+  let mim = Math.max(90, control.minimal);
+  if(mn < mim) mn = mim;
+  // mn/=20;
+  
+  for(let x = 0; x < imagedata.width; x += mn) {
+    for(let y = 0; y < imagedata.height; y += mn) {
+      paintGrid(imagedata, x, y, mn, mn);
+    }
+  }
   ARCH.refreshSelection(scene);
 }
 
-
-/* ---------- animate per frame ---------- */
-function draw() {
-
+const control = {
+  edge:false,
+  threshold:0.3,
+  minimal:9,
+  update:function (){
+    clear();
+    generateGrid(image);
+  }
 }
 
 
@@ -112,13 +135,18 @@ function main() {
   viewport.enableDragFrames();
   viewport.enableTransformer();
   
-  initScene();
-  
-  viewport.draw = draw;
-  
   const sceneBasic = new ARCH.SceneBasic(scene, renderer);
   sceneBasic.addGUI(gui);
-  initGUI();
+  sceneBasic.floorColor = '#ffffff';
+  sceneBasic.axes = false;
+  sceneBasic.shadow = false;
+  sceneBasic.update();
+  
+  gui.add(control, 'edge');
+  gui.add(control, 'threshold', 0, 1);
+  gui.add(control, 'minimal', 1, 100, 1);
+  gui.add(control, 'update');
+  initScene();
 }
 
 export {
